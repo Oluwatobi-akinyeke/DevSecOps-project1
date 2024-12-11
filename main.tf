a046ec7e973513f8c08daf6c9863965242b6d1da
oluwatobi-akinyeke <oluwatobiakinyeke@yahoo.com>
2024-12-11 00:08:43 -0500

    update

--- main.tf
+++ main.tf
@@ -3,3 +3,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraform-december-project01" {
+  bucket = "terraform-december-project01"
+  }
+  terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-december-project01"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
